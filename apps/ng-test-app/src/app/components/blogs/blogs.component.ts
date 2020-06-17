8bc265ee1b1863bf5c2d5ec1761bf0b717a542b4
Jamie King <jamie.king@goodthingsfoundation.org>
2020-06-17 09:11:40 +0100

    testing using angular meta service

--- apps/ng-test-app/src/app/components/blogs/blogs.component.ts
+++ apps/ng-test-app/src/app/components/blogs/blogs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { blogsSelector } from '../../common/store/app.selectors';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'nx-ng-nest-universal-blogs',
@@ -11,7 +12,9 @@ export class BlogsComponent implements OnInit {
 
   public blogs;
 
-  constructor(private store:Store) { }
+  constructor(private store:Store, private meta:Meta) { 
+    this.meta.addTags([{ name: 'og:title', content: 'this is the blog page' }])
+  }
 
   ngOnInit(): void {
     this.blogs = this.store.select(blogsSelector);
