2cc42a4b21f2ca156dcb367a9a3378a4240d1afd
Jamie King <jamie.king@goodthingsfoundation.org>
2020-06-17 10:30:56 +0100

    opengraph tags on blog component

--- apps/ng-test-app/src/app/components/blogs/blogs.component.ts
+++ apps/ng-test-app/src/app/components/blogs/blogs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { blogsSelector } from '../../common/store/app.selectors';
 import { Meta } from '@angular/platform-browser';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'nx-ng-nest-universal-blogs',
@@ -12,8 +13,13 @@ export class BlogsComponent implements OnInit {
 
   public blogs;
 
-  constructor(private store:Store, private meta:Meta) { 
-    this.meta.addTags([{ name: 'og:title', content: 'this is the blog page' }])
+  constructor(private store:Store, private meta:Meta, private location:Location) { 
+    this.meta.addTags([
+      { name: 'og:type', content: 'website' },
+      { name: 'og:title', content: 'this is the blog page' },
+      { name: 'og:image', content: 'https://www.goodthingsfoundation.org/sites/all/themes/gtf2018/images/site-logo.png' },
+      { name: 'og:url', content: this.location['_platformLocation']['_doc']['_address'] }
+    ]);
   }
 
   ngOnInit(): void {
